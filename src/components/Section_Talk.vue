f030f4a413dfd56bdcd0b2a96f32ed0558f1f970
TonyBrb <anthonybarbagallo95@gmail.com>
2021-12-15 09:34:28 +0100

    quinta section main

--- src/components/Section_Talk.vue
+++ src/components/Section_Talk.vue
@@ -1,7 +1,10 @@
 <template>
   <section>
-    <h1>Ready to talk?</h1>
-    <span>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus, beatae? Exercitationem magni repellendus itaque minus commodi, sint error magnam eligendi ipsam suscipit ullam corporis libero earum assumenda fugit quaerat pariatur?</span>
+    <div class="container">
+      <h1>Ready to talk?</h1>
+      <span>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus, beatae? Exercitationem magni repellendus itaque</span>
+      <button class="btn">LET'S TALK</button>
+    </div>
   </section>
 </template>
 
@@ -13,8 +16,34 @@ export default {
 
 <style lang="scss" scoped>
   section{
-    height: 400px;
+    height: 300px;
     background-image: url('../assets/img/ready_to_talk_cta_bg.jpg');
     object-fit: contain;
+    text-align: center;
+    color: white;
+    .container{
+      width: 30%;
+      margin: 0 auto;
+    }
+
+    h1{
+      margin: 0;
+      font-size: 50px;
+      padding-top: 80px;
+      padding-bottom: 20px;
+    }
+    
+    .btn{
+      margin-top: 20px;
+      background-color: #5cdbcc;
+      padding: 10px 20px 10px 20px;
+      border: none;
+      border-radius: 20px;
+      color: white;
+      font-size: 12px;
+      cursor: pointer;
+      
+    }
+
   }
 </style>
